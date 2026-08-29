119999db79b7411f17c8c914261eda73c3b0c164
zeekreya <shaiksami963@gmail.com>
2026-08-29 20:22:54 +0530

    changes for render

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -7,23 +8,32 @@ const router = require('./router/userRouter');
 const examRouter = require('./router/examRouter');
 const questionRouter = require('./router/questionRouter');
 const resultRouter = require('./router/resultRouter');
+
 const app = express();
 
-app.use(cors("*"));
+app.use(cors());
 app.use(express.json());
 
-
-mongoose.connect(process.env.DB_URL).then(() => {
-    console.log("MongoDb connected successfully ");
-}).catch((error) =>{
-    console.log("MongoDb connection failed",error );
-});
+mongoose
+    .connect(process.env.DB_URL)
+    .then(() => {
+        console.log('MongoDb connected successfully');
+    })
+    .catch((error) => {
+        console.log('MongoDb connection failed', error);
+    });
 
 app.use('/user', router);
 app.use('/exam', examRouter);
 app.use('/question', questionRouter);
 app.use('/result', resultRouter);
 
-app.listen(process.env.PORT, () => {
-    console.log(`server is running on port ${process.env.PORT}`);
+app.get('/', (req, res) => {
+    res.send('Online Exam Backend is running');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
